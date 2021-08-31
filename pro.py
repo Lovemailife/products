da0b11b304d7141c5213cfc44134ad2586aacb50
Kristin Chen <coolfe184770@gmail.com>
2021-08-30 21:50:56 -0500

    add read file

--- pro.py
+++ pro.py
@@ -1,4 +1,14 @@
-#記帳程式
+#讀取檔案
+products=[]
+with open ('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        if '商品,價格' in line:
+            continue #跳過這回合
+        name,price=line.strip().split(',') #split切割,遇到逗點就切割;strip去掉空格及換行符號
+        products.append([name, price])
+print(products)
+
+#讓消費者輸入
 products=[]
 while True:
     name =input('商品名:')
@@ -8,13 +18,14 @@ while True:
     p = [name, price]
     products.append(p)
 print(products)    
-
 print(products[0][0]) #大清單中第一個清單的第一個位置
 
+#印出所有購買紀錄
 for p in products:
     #print(p)
     print(p[0],'的價格是',p[1], '元')
 
+#寫入檔案
 with open ('products.csv', 'w', encoding='utf-8') as f :
     f.write('商品, 價格\n')
     for p in products:
